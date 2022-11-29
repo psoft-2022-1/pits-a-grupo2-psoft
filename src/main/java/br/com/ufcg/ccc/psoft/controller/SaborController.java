802eaa59f4b33553c6266b9e25a3928531246167
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2022-11-29 14:15:55 -0300

    consultarSabor()

--- src/main/java/br/com/ufcg/ccc/psoft/controller/SaborController.java
+++ src/main/java/br/com/ufcg/ccc/psoft/controller/SaborController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ufcg.ccc.psoft.dto.SaborDTO;
 import br.com.ufcg.ccc.psoft.exception.SaborAlreadyCreatedException;
+import br.com.ufcg.ccc.psoft.exception.SaborNotFoundException;
 import br.com.ufcg.ccc.psoft.service.SaborService;
 import br.com.ufcg.ccc.psoft.util.ErroSabor;
 
@@ -30,6 +33,17 @@ public class SaborController {
 			return new ResponseEntity<SaborDTO>(sabor, HttpStatus.CREATED);
 		} catch (SaborAlreadyCreatedException e) {
 			return ErroSabor.erroSaborJaCadastrado(saborDTO);
+		}
+	}
+	
+	@GetMapping(value = "/sabor/{idSabor}")
+	public ResponseEntity<?> consultarSabor(@PathVariable("idSabor") long idSabor) {
+
+		try {
+			SaborDTO sabor = saborService.getSaborById(idSabor);
+			return new ResponseEntity<SaborDTO>(sabor, HttpStatus.OK);
+		} catch (SaborNotFoundException e) {
+			return ErroSabor.erroSaborNaoEncontrado(idSabor);
 		}
 	}
 }
