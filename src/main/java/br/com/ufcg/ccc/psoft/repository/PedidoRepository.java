d1369f37c77b987864337baeb233b397e74be332
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2023-01-03 17:31:38 -0300

    att Repositories

--- src/main/java/br/com/ufcg/ccc/psoft/repository/PedidoRepository.java
+++ src/main/java/br/com/ufcg/ccc/psoft/repository/PedidoRepository.java
@@ -1,18 +1,8 @@
 package br.com.ufcg.ccc.psoft.repository;
 
-import br.com.ufcg.ccc.psoft.dto.responses.PedidoResponseDTO;
-import br.com.ufcg.ccc.psoft.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.ufcg.ccc.psoft.model.Pedido;
 
-import java.util.List;
-
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-
-    PedidoResponseDTO findPedidoByClienteAndId(Cliente cliente, Long idPedido);
-
-    List<PedidoResponseDTO> findPedidosByClienteOrderByIdDesc(Cliente cliente);
-
-    List<PedidoResponseDTO> findPedidosByClienteAndStatusPedidoOrderByIdDesc(Cliente cliente, String status);
 
 }
