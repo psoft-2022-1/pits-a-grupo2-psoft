1c9559009c86adf6d152e888ba6c54d556f6c1bf
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2023-01-03 16:57:21 -0300

    att Exceptions

---
+++ src/main/java/br/com/ufcg/ccc/psoft/exception/NaoHaEntregadoresDisponiveisException.java
@@ -0,0 +1,9 @@
+package br.com.ufcg.ccc.psoft.exception;
+
+public class NaoHaEntregadoresDisponiveisException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8897873524833802691L;
+}
